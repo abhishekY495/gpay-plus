e94b1319f876314f0d4830b1a0cd5da9f77c664e
Abhishek <abhishekY495.sec@gmail.com>
2024-02-07 23:18:01 +0530

    Refactor

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,10 +4,10 @@ const deployedApiUrl = import.meta.env.VITE_DEPLOYED_API_URL;
 
 export const API_URL = env === "PROD" ? deployedApiUrl : localApiUrl;
 export const guestCredentials = { username: "guest", password: "guest12345" };
-export const maxCookieAge = 30 * 24 * 60 * 60 * 1000;
 export const cookieDefaultOptions = {
   path: "/",
   sameSite: "strict",
-  maxAge: maxCookieAge,
-  domain: env === "PROD" ? "https://gpay-plus.netlify.app" : "localhost",
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  secure: env === "PROD",
+  domain: env === "PROD" ? "gpay-plus.netlify.app" : "localhost",
 };
