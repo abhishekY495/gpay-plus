60c739c99170346c8b4402d10e8deffd2a3ced0a
Abhishek <abhishekY495.sec@gmail.com>
2024-02-11 20:34:43 +0530

    Fix: Updated guest credentials

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,4 +3,7 @@ const localApiUrl = import.meta.env.VITE_LOCAL_API_URL;
 const deployedApiUrl = import.meta.env.VITE_DEPLOYED_API_URL;
 
 export const API_URL = env === "PROD" ? deployedApiUrl : localApiUrl;
-export const guestCredentials = { username: "guest", password: "guest12345" };
+export const guestCredentials = {
+  username: "guest_testing",
+  password: "guest12345",
+};
