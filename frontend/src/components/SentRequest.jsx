2eab5ad4570cf889152222b20832ad6e538274e6
Abhishek <abhishekY495.sec@gmail.com>
2024-02-17 10:13:58 +0530

    Styling

--- frontend/src/components/SentRequest.jsx
+++ frontend/src/components/SentRequest.jsx
@@ -8,28 +8,30 @@ export const SentRequest = ({ request }) => {
 
   const statusBgColor =
     status === "PENDING"
-      ? "bg-blue-400"
+      ? "bg-blue-100/20"
       : status === "PAID"
-      ? "bg-green-400"
-      : "bg-red-400";
+      ? "bg-green-100/20"
+      : "bg-red-100/20";
+
+  const textColor =
+    status === "PENDING"
+      ? "text-blue-500"
+      : status === "PAID"
+      ? "text-green-500"
+      : "text-red-500";
 
   return (
-    <div className="flex justify-between items-center odd:bg-neutral-100 p-2 pb-3 px-4 rounded-md hover:cursor-pointer">
+    <div
+      className={`flex justify-between items-center ${statusBgColor} p-2 pb-3 px-4 rounded-md hover:cursor-pointer`}
+    >
       <div>
         <p className="text-xl font-bold max-[430px]:text-lg">{fullname}</p>
         <p className="text-sm -mt-1">@{username}</p>
         <p className="text-sm">{formatDate(createdAt)}</p>
       </div>
-      <div className="flex flex-col items-center">
-        <p className="text-lg font-semibold max-[430px]:text-base">
-          {formatAmount(amount)}
-        </p>
-        <span
-          className={`${statusBgColor} px-5 py-1 rounded font-semibold hover:cursor-pointer hover:opacity-90`}
-        >
-          {status}
-        </span>
-      </div>
+      <p className={`text-lg ${textColor} font-semibold max-[430px]:text-base`}>
+        {formatAmount(amount)}
+      </p>
     </div>
   );
 };
